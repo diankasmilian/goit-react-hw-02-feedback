765d0daacac9dfe418b3af67db832eb55cd01196
Diana Smilian <dianka.kulakevich@gmail.com>
2023-06-23 13:02:18 +0300

    finish step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import React, {Component} from "react";
+import {Container, ButtonList, StatisticList} from './App.styled'
+
+class App extends Component {
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+
+handleClick = e => {
+  const {name} = e.target;
+
+  this.setState(prevState => ({
+    [name]: prevState[name] + 1,
+  })
+  )
+  console.log(e.target);
+  console.log(this.state.good)
+}
+render() {
+return (
+  <Container>
+    <div>
+      <h2>Please leave feedback</h2>
+      <ButtonList>
+        <li>
+          <button type="button" name="good" onClick={this.handleClick}>Good</button>
+        </li>
+        <li>
+          <button type="button" name="neutral" onClick={this.handleClick}>Neutral</button>
+        </li>
+        <li>
+          <button type="button" name="bad" onClick={this.handleClick}>Bad</button>
+        </li>
+      </ButtonList>
     </div>
-  );
-};
+    <div>
+    <h2>Statisytics</h2>
+      <StatisticList>
+        <li>
+          <p>Good: {this.state.good} </p>
+        </li>
+        <li>
+          <p>Neutral: {this.state.neutral}</p>
+        </li>
+        <li>
+          <p>Bad: {this.state.bad}</p>
+        </li>
+      </StatisticList>
+    </div>
+  </Container>
+)
+}
+}
+
+export default App;
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+margin-left: 20px;
+`
+
+export const ButtonList = styled.ul`
+list-style: none;
+display: flex;
+gap: 15px;
+padding: 0;
+`
+
+export const StatisticList = styled.ul`
+list-style: none;
+padding: 0;
+`
